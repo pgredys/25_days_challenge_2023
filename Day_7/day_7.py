e128e878a835578081cc319b0a5fe40da23011a2
Przemysław Grędys <gredys.przemyslaw@gmail.com>
2023-12-07 23:45:34 +0100

    Day 7 - part 2

--- Day_7/day_7.py
+++ Day_7/day_7.py
@@ -1,6 +1,6 @@
 from functools import cmp_to_key
 
-cards_order = ('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
+cards_order = ('A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J')
 
 
 def load_input(path: str) -> list:
@@ -28,6 +28,24 @@ def read_hands(path: str) -> list[str, int]:
     return hands_list
 
 
+def contains_joker(card_set: str):
+    if card_set.find('J') >= 0:
+        reference_rank = get_rank(card_set)
+        letter_reference = ''
+        for letter in cards_order:
+            if get_rank(card_set.replace('J', letter)) > reference_rank:
+                letter_reference = letter
+                reference_rank = get_rank(card_set.replace('J', letter))
+
+        if get_rank(card_set) < reference_rank:
+            return card_set.replace('J', letter_reference)
+        else:
+            return card_set
+
+    else:
+        return card_set
+
+
 def get_rank(card_set: str):
     counter = dict((k, card_set.count(k)) for k in cards_order if card_set.count(k) != 0)
 
@@ -53,7 +71,11 @@ def get_rank(card_set: str):
 def compare_by_numbers(item1: str, item2: str):
     assert len(item1) == len(item2)
 
+    if item1 == item2:
+        return 0
+
     for letter_index, _ in enumerate(item1):
+
         if card_strength(item1[letter_index]) > card_strength(item2[letter_index]):
             return 1
         if card_strength(item1[letter_index]) < card_strength(item2[letter_index]):
@@ -61,9 +83,12 @@ def compare_by_numbers(item1: str, item2: str):
 
 
 def compare(item1, item2):
-    if get_rank(item1) < get_rank(item2):
+    Item1 = contains_joker(item1)
+    Item2 = contains_joker(item2)
+
+    if get_rank(Item1) < get_rank(Item2):
         return -1
-    elif get_rank(item1) > get_rank(item2):
+    elif get_rank(Item1) > get_rank(Item2):
         return 1
     else:
         return compare_by_numbers(item1, item2)
@@ -71,15 +96,17 @@ def compare(item1, item2):
 
 def first_answer(path: str) -> int:
     hands = read_hands(input_path)
+
     hands_list = [i[0] for i in hands]
     hands_reference = [i[0] for i in hands]
     hands_bid = [i[1] for i in hands]
+
     hands_list.sort(key=cmp_to_key(compare))
 
     result = 0
     for i, item in enumerate(hands_list):
         index = hands_reference.index(item)
-        result += (i+1)*hands_bid[index]
+        result += (i + 1) * hands_bid[index]
 
     return result
 
